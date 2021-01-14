bbeac70d98149702958cfe44cbc12a0dbc2276e0
andabil <ahmon_abilar@yahoo.com>
2021-01-14 17:27:58 +0000

    Add implementation for calculator service

--- src/Services/CalculatorService.cs
+++ src/Services/CalculatorService.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Calculator.Services.Interfaces;
 
 namespace Calculator.Services
 {
-    class CalculatorService
+    public class CalculatorService: ICalculator
     {
+        /// <summary>
+        /// Adds first and second parameters
+        /// </summary>
+        /// <param name="first">First number to be added</param>
+        /// <param name="second">Second number to be added</param>
+        /// <returns>Sum of first and second number</returns>
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        /// <summary>
+        /// Subtracts first and second parameters
+        /// </summary>
+        /// <param name="first">Used as the minuend</param>
+        /// <param name="second">Used as the subtrahend</param>
+        /// <returns>Difference between first and second number</returns>
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        /// <summary>
+        /// Multiplies first param by second param
+        /// </summary>
+        /// <param name="first">Used as the multiplicand</param>
+        /// <param name="second">Used as the multiplier</param>
+        /// <returns>Product of first and second numbers</returns>
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+        /// <summary>
+        /// Divides dividend by divisor
+        /// </summary>
+        /// <param name="dividend">Used as dividend in the divide operation</param>
+        /// <param name="divisor">Used as divisor in the divide operation</param>
+        /// <returns>Quotient</returns>
+        public double Divide(int dividend, int divisor)
+        {
+            return (double)dividend / divisor;
+        }
     }
 }
