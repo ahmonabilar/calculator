d511befc55828268024ed6cad896e0e8e2ef67ba
andabil <ahmon_abilar@yahoo.com>
2021-01-14 17:17:13 +0000

    Initial calculator service

---
+++ src/Services/Interfaces/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Services.Interfaces
+{
+    public interface ICalculator
+    {
+        int Add(int first, int second);
+        int Subtract(int first, int second);
+        int Multiply(int first, int second);
+        double Divide(int dividend, int divisor);
+    }
+}
