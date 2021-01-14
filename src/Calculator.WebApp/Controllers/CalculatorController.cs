e98c355cf72e3e3f01e8099634b3d89260ab24b8
andabil <ahmon_abilar@yahoo.com>
2021-01-14 22:57:03 +0000

    Add web application

---
+++ src/Calculator.WebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Calculator.Services.Interfaces;
+using Calculator.WebApp.Models;
+
+namespace Calculator.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ICalculator _calculatorService;
+
+        public CalculatorController(ICalculator calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        [HttpPost]
+        public string Post(InputModel input)
+        {
+            var result = string.Empty;
+            switch (input.Operator)
+            {
+                case '+':
+                    result = _calculatorService.Add(input.First, input.Second).ToString();
+                    break;
+                case '-':
+                    result = _calculatorService.Subtract(input.First, input.Second).ToString();
+                    break;
+                case '*':
+                    result = _calculatorService.Multiply(input.First, input.Second).ToString();
+                    break;
+                case '/':
+                    var quotient = _calculatorService.Divide(input.First, input.Second);
+
+                    result = double.IsInfinity(quotient) ? $"Infinite" : quotient.ToString("0.##");
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Calculator.WebApp/Models/InputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.WebApp.Models
+{
+    public class InputModel
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public char Operator { get; set; }
+    }
+}
